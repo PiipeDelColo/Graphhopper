35571f378e6c6a127bde859a1e4ab8231f84da2a
PiipeDelColo <piipe.valladares@gmail.com>
2024-05-03 03:42:06 +0000

    Archivos de python graphhopper gps

---
+++ graphhopper_parse-json_1.py
@@ -0,0 +1,15 @@
+import requests
+import urllib.parse
+
+geocode_url = "https://graphhopper.com/api/1/geocode?"
+route_url = "https://graphhopper.com/api/1/route?"
+loc1 = "Washington, D.C."
+loc2 = "Baltimore, Maryland"
+key = "949103ea-5674-4d8d-9020-c55e9f7d40ca"
+
+url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+
+replydata = requests.get(url)
+json_data = replydata.json()
+json_status = replydata.status_code
+print(json_data)
